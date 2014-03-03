cf314de55277b272607431f4d0684059a7a8e7bb
German Bringas <germanbringas@gmail.com>
2014-03-02 23:03:09 -0300

    test post

--- js/script.js
+++ js/script.js
@@ -57,7 +57,7 @@
 				MELI.init({client_id: 8258968359213576});
 				MELI.login(function() {
 					var token = MELI.getToken();
-					var jsonToPost =  {'items': [{'id': '123','title': $scope.description, 'quantity': 1, 'unit_price': $scope.partialAmount, 'currency_id': 'ARS', 'picture_url': 'http://hagamosunavaquita.com.ar/cowww.png'}]};
+					var jsonToPost =  {'items': [{'id': '123','title': $scope.description, 'quantity': 1, 'unit_price': parseFloat($scope.partialAmount), 'currency_id': 'ARS', 'picture_url': 'http://hagamosunavaquita.com.ar/cowww.png'}]};
 	    			var url = "/checkout/preferences";
 					MELI.post(url, jsonToPost, function(data) {
 						console.log(data);
